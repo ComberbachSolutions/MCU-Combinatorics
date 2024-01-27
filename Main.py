4afb924bc34382537ddf0d2bf6b4aac8c6f4619d
Craig Comberbach <craig.comberbach@gmail.com>
2024-01-27 14:57:42 -0600

    Refactored removal functions to make them simpler to call

--- Main.py
+++ Main.py
@@ -51,35 +51,15 @@ MCU_Pins = [
             "Subfeature C":
             [
                 "Channel 1",
-                "DeleteMe"
-            ],
-        },
-    },
-    {
-        "Feature 4":
-        {
-            "Subfeature C":
-            [
-                "Channel 1",
-                "Channel 2",
-            ],
-            "Subfeature DeleteMe":
-            [
-                "Channel 3",
             ],
         },
     },
     {
         "Feature DeleteMe":
         {
-            "Subfeature C":
+            "Subfeature DeleteMe":
             [
-                "Channel 1",
-                "Channel 2",
-            ],
-            "Subfeature A":
-            [
-                "Channel 3",
+                "DeleteMe"
             ],
         },
     },
@@ -113,24 +93,27 @@ def Solve(Requirements, Pins):
 def Remove_Channel(PinMap, Feature, SubFeature, Channel):
     PinMap[Feature][SubFeature].remove(Channel)
 
-def Remove_Subfeature(PinMap, Feature, SubFeature):
-    del PinMap[Feature][SubFeature]
+def Remove_Channel(ChannelMap, Channel):
+    ChannelMap.remove(Channel)
+
+def Remove_Subfeature(SubfeatureMap, SubFeature):
+    del SubfeatureMap[SubFeature]
 
 def Remove_Feature(PinMap, Feature):
     del PinMap[Feature]
 
 print("*"*50)
-print(MCU_Pins[2])
-Remove_Channel(MCU_Pins[2], "Feature 3", "Subfeature C", "DeleteMe")
-print(MCU_Pins[2])
-print("*"*50)
 print(MCU_Pins[3])
-Remove_Subfeature(MCU_Pins[3], "Feature 4", "Subfeature DeleteMe")
+Remove_Channel(MCU_Pins[3]["Feature DeleteMe"]["Subfeature DeleteMe"], "DeleteMe")
 print(MCU_Pins[3])
 print("*"*50)
-print(MCU_Pins[4])
-Remove_Feature(MCU_Pins[4], "Feature DeleteMe")
-print(MCU_Pins[4])
+print(MCU_Pins[3])
+Remove_Subfeature(MCU_Pins[3]["Feature DeleteMe"], "Subfeature DeleteMe")
+print(MCU_Pins[3])
+print("*"*50)
+print(MCU_Pins[3])
+Remove_Feature(MCU_Pins[3], "Feature DeleteMe")
+print(MCU_Pins[3])
 print("*"*50)
 
 Solve(Requirements, MCU_Pins)
